16558cddb9b7dfb2852621ad4f0fdc03ae464f79
Alvin Yap <leaf.alvin@gmail.com>
2023-10-13 17:03:15 +0800

    feat: deck

--- game/Deck.js
+++ game/Deck.js
@@ -1,3 +1,5 @@
+const { createHash, randomBytes } = require("crypto");
+
 class Deck {
   /*** Constructor Function***/
   constructor() {
@@ -32,8 +34,40 @@ class Deck {
   }
 
   /*** Shuffle deck using Fisher-Yates Algorithm ***/
+  /* Math.ramdom() is pseudoramdom */
+  /* Depends of browser, it will use different seed to generate the randomness */
+  shuffle() {
+    for (let i = this.sequence.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.sequence[i], this.sequence[j]] = [
+        this.sequence[j],
+        this.sequence[i],
+      ];
+    }
+  }
+
+  /*** Hash the current sequence with salt and return the hash ***/
+  hashState() {
+    let sequence = "";
+    for (let card of this.deckSequnce) {
+      sequence += card;
+    }
+    let salt = randomBytes(32).toString("hex");
+    this.verifySequence = sequence;
+    this.verifySalt = salt;
+
+    return createHash("sha256")
+      .update(salt + sequence)
+      .digest("hex");
+  }
+
+  /* Deal a card from the top of the deck */
+  deal() {
+    const card = this.sequence.shift();
+    return card;
+  }
 }
 
 /*** Check whether the deck is propally initialized ***/
-// const myDeck = new Deck();
-// console.log(myDeck.deckSequnce);
+const myDeck = new Deck();
+console.log(myDeck.deckSequnce);
