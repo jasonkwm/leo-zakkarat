d0b851a6d9828464ee74bc61d67c90fab7516bb2
hiromasa <howzheng0629@gmail.com>
2023-10-14 12:54:34 +0800

    frontend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraBaseProvider, ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraBaseProvider,
+  ChakraProvider,
+  extendTheme,
+} from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      zakkarat_background: "#131312",
+      zakkarat_white: "#f5f5f5",
+      zakkarat_black: "#121212",
+    },
+  },
+  fonts: {
+    heading: "Azeret Mono, monospace",
+    body: "Azeret Mono, monospace",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
