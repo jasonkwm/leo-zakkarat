f52745a4cf6c68906a1152790c3b165502016df0
Jason <jasonkwm13@gmail.com>
2023-10-15 03:08:24 +0800

    fix: baccarat table look

--- frontend/src/components/BaccaratTable.tsx
+++ frontend/src/components/BaccaratTable.tsx
@@ -5,21 +5,21 @@ export default function BaccaratTable() {
     const { betOn, setBetOn, selectedTable } = useMainDataContext();
     return (
         <>
-            <Text color={"black"} position={"absolute"} left={"35%"} top={"2.5%"}>
+            <Text color={"white"} position={"absolute"} left={"35%"} top={"2.5%"}>
                 Table No: {selectedTable}, Bets On: {betOn}
             </Text>
             <Button
                 position={"absolute"}
-                top={"20%"}
-                left={"43%"}
+                top={"19.70%"}
+                left={"42.75%"}
                 borderRadius={"50%"}
-                width={"124px"}
-                height={"124px"}
+                width={"128px"}
+                height={"128px"}
                 background={"transparent"}
                 border={"solid 1px yellow"}
                 sx={{
                     "&:hover": {
-                        transform: "scale(1.1)",
+                        transform: "scale(1.15)",
                         backgroundColor: "#FFFFFF1A",
                         border: "solid 1px yellow",
                     },
@@ -31,11 +31,11 @@ export default function BaccaratTable() {
             ></Button>
             <Button
                 position={"absolute"}
-                top={"26.5%"}
-                left={"3.8%"}
+                top={"25.5%"}
+                left={"3.2%"}
                 borderRadius={"50%"}
-                width={"330px"}
-                height={"330px"}
+                width={"340px"}
+                height={"340px"}
                 background={"transparent"}
                 border={"solid 1px yellow"}
                 sx={{
@@ -52,11 +52,11 @@ export default function BaccaratTable() {
             ></Button>
             <Button
                 position={"absolute"}
-                top={"26.5%"}
-                right={"3.9%"}
+                top={"26%"}
+                right={"3.4%"}
                 borderRadius={"50%"}
-                width={"330px"}
-                height={"330px"}
+                width={"340px"}
+                height={"340px"}
                 background={"transparent"}
                 border={"solid 1px yellow"}
                 sx={{
